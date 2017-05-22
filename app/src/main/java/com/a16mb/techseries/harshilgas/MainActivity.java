780cdbe9e705b7a41b25b82f174369328fcbf81c
Yhdesai <yhdesai99@ymail.com>
2017-05-22 18:46:14 +0530

    initial commit

--- app/src/main/java/com/a16mb/techseries/harshilgas/MainActivity.java
+++ app/src/main/java/com/a16mb/techseries/harshilgas/MainActivity.java
@@ -1,13 +1,45 @@
 package com.a16mb.techseries.harshilgas;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
+
     }
+
+    public void addListenerOnButton() {
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:" + Uri.encode(PhoneNum.trim())));
+                callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(callIntent);
+
+
+            }
+
+        });
+
+    }
+
 }
